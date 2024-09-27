294b9f0c9136ca98227ece2a93f9af74d05a0035
Anton V. Gorshkov <avgorshk@users.noreply.github.com>
2024-09-27 19:26:10 +0300

    Update block_gemm_cuda.h

--- 3821B1FI3/6_block_gemm_cuda/kuznetsov_artyom/block_gemm_cuda.h
+++ 3821B1FI3/6_block_gemm_cuda/kuznetsov_artyom/block_gemm_cuda.h
@@ -5,6 +5,6 @@
 #include <vector>
 
 std::vector<float> BlockGemmCUDA(const std::vector<float>& a,
-                                 const std::vector<float>& b, int size);
+                                 const std::vector<float>& b, int n);
 
 #endif  // __BLOCK_GEMM_CUDA_H
