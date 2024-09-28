19b742e88fe965995ce97546bb23febacd727488
Zhenya_Kulaev <kylaev11@gmail.com>
2024-09-28 22:45:27 +0300

    first commit

---
+++ 3821B1FI3/1_gelu_omp/kulaev_zhenya/gelu_omp.cpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2024 Kulaev Zhenya
+#include "gelu_omp.h"
+
+#include <cmath>
+
+std::vector<float> GeluOMP(const std::vector<float> &input) {
+  if (input.empty()) return {};
+
+  std::vector<float> output(input.size());
+
+  const float constOne = 0.044715;
+  const float constTwo = std::sqrt(2.0 / M_PI);
+
+#pragma omp parallel for
+  for (size_t i = 0; i < input.size(); i++) {
+    float x = input[i];
+    float tanh_arg = constTwo * (x + constOne * std::pow(x, 3));
+    output[i] = 0.5 * x * (1.0 + std::tanh(tanh_arg));
+  }
+
+  return output;
+}
