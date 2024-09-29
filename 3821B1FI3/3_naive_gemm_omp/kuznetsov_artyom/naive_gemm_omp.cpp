fb7c00045b7f58cea87e7b6fdc6e982a7fcb1f5b
Kuznetsov-Artyom <artyomka_working@mail.ru>
2024-09-29 18:23:18 +0300

    refactoring

--- 3821B1FI3/3_naive_gemm_omp/kuznetsov_artyom/naive_gemm_omp.cpp
+++ 3821B1FI3/3_naive_gemm_omp/kuznetsov_artyom/naive_gemm_omp.cpp
@@ -3,18 +3,15 @@
 
 std::vector<float> NaiveGemmOMP(const std::vector<float>& a,
                                 const std::vector<float>& b, int size) {
-  std::size_t countElem = size * size;
+  auto countElem = size * size;
   if (a.size() != countElem || b.size() != countElem) return {};
 
   std::vector<float> c(countElem, 0.0f);
-  std::size_t m = 0;
-  std::size_t n = 0;
-  std::size_t k = 0;
 
-#pragma omp parallel for shared(a, b, c) private(m, n, k) collapse(3)
-  for (m = 0; m < size; ++m)
-    for (n = 0; n < size; ++n)
-      for (k = 0; k < size; ++k)
+#pragma omp parallel for shared(a, b, c)
+  for (int m = 0; m < size; ++m)
+    for (int n = 0; n < size; ++n)
+      for (int k = 0; k < size; ++k)
         c[m * size + n] += a[m * size + k] * b[size * k + n];
 
   return c;
