683f76252a9a4a476cabf94ed71ed597be1999cf
unknown <simonyan-junior@yandex.ru>
2024-10-05 11:36:05 +0300

    First commit

--- 3821B1FI3/2_gelu_cuda/simonyan_suren/gelu_cuda.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024 Simonyan Suren
-#ifndef __GELU_CUDA_H
-#define __GELU_CUDA_H
-
-#include <vector>
-
-std::vector<float> GeluCUDA(const std::vector<float>& input);
-
-#endif  // __GELU_CUDA_H
