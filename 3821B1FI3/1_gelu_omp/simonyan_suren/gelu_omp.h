6f984980aa04e7297587adabceaa69c3731123f8
unknown <simonyan-junior@yandex.ru>
2024-10-05 11:08:02 +0300

    Updated results

--- 3821B1FI3/1_gelu_omp/simonyan_suren/gelu_omp.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024 Simonyan Suren
-#ifndef __GELU_OMP_H
-#define __GELU_OMP_H
-
-#include <vector>
-
-std::vector<float> GeluOMP(const std::vector<float>& input);
-
-#endif // __GELU_OMP_H
