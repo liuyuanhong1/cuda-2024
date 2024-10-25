50f58ad5c1b9c13cefaddc6dbefe4aa5aeee354d
overinvest <r1gzhvhkid@gmail.com>
2024-10-25 15:58:59 +0300

    Added lab 3

---
+++ 3821B1FI1/3_naive_gemm_omp/bodrov_daniil/naive_gemm_omp.cpp
@@ -0,0 +1,38 @@
+/*General matrix multiplication (GEMM) is a very basic and broadly used linear algebra operation applied in high performance computing (HPC), statistics, deep learning and other domains. There are a lot of GEMM algorithms with different mathematical complexity form $O(n^3)$ for naive and block approaches to $O(n^{2.371552})$ for the method descibed by Williams et al. in 2024 [[1](https://epubs.siam.org/doi/10.1137/1.9781611977912.134)]. But despite a variety of algorithms with low complexity, block matrix multiplication remains the most used implementation in practice since it fits to modern HW better.
+
+To start learning matrix multiplication smoother, let us start with naive approach here. To compute matrix multiplication result C for matricies A and B, where C = A * B and the size for all matricies are $n*n$, one should use the following formula for each element of C (will consider only square matricies for simplicity):
+
+$c_{ij}=\sum_{k=1}^na_{ik}b_{kj}$
+
+To complete the task one should implement a function that multiplies two square matricies using OpenMP with the following interface:
+```cpp
+std::vector<float> NaiveGemmOMP(const std::vector<float>& a,
+                                const std::vector<float>& b,
+                                int n);
+```
+Each matrix must be stored in a linear array by rows, so that `a.size()==n*n`. Function takes two matricies and their size as inputs, and returns result matrix also stored by rows.
+
+For simplicity, let's consider matrix size is always power of 2.
+*/
+
+#include "naive_gemm_omp.h"
+#include <omp.h>
+
+std::vector<float> NaiveGemmOMP(const std::vector<float>& a,
+                                const std::vector<float>& b,
+                                int n) {
+    std::vector<float> c(n * n, 0.0f);
+
+    #pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            float sum = 0.0f;
+            for (int k = 0; k < n; ++k) {
+                sum += a[i * n + k] * b[k * n + j];
+            }
+            c[i * n + j] = sum;
+        }
+    }
+
+    return c;
+}
