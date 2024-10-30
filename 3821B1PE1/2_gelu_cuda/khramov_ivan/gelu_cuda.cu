acc296d97e62d13fd4f5af267ae8eb8af7d2202b
At0mn1yIvan <khramov.vanya@bk.ru>
2024-10-30 20:32:11 +0300

    Task commit: task done.

--- 3821B1PE1/2_gelu_cuda/khramov_ivan/gelu_cuda.cu
+++ 3821B1PE1/2_gelu_cuda/khramov_ivan/gelu_cuda.cu
@@ -1,5 +1,46 @@
+// Copyright (c) 2024 Khramov Ivan
 #include "gelu_cuda.h"
 
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <cuda.h>
+#include <cstdlib>
+#include <cmath>
+
+__global__ void geluKernel(const float* input, float* result, size_t size) {
+    
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    const float geluConst = 0.044715f;
+
+    if (idx < size) {
+        float x = input[idx];
+        result[idx] = 0.5f * x * (1.0f + tanh(sqrt(2.0f / M_PI)
+         * (x + geluConst * x * x * x)));
+    }
+}
+
 std::vector<float> GeluCUDA(const std::vector<float>& input) {
-    // Place your implementation here
+    size_t vectorSize = input.size();
+    std::vector<float> result(vectorSize);
+    size_t sizeInBytes = vectorSize * sizeof(*input.data());
+
+    float* dInput;
+    float* dOutput;
+    cudaMalloc(&dInput, sizeInBytes);
+    cudaMalloc(&dOutput, sizeInBytes);
+
+    cudaMemcpy(dInput, input.data(), sizeInBytes, cudaMemcpyHostToDevice);
+
+    cudaDeviceProp deviceProps;
+    cudaGetDeviceProperties(&deviceProps, 0);
+    size_t threadsBlock = deviceProps.maxThreadsPerBlock;
+    size_t blocksGrid = (vectorSize + threadsBlock - 1) / threadsBlock;
+
+    geluKernel<<<blocksGrid, threadsBlock>>>(dInput, dOutput, vectorSize);
+    cudaMemcpy(result.data(), dOutput, sizeInBytes, cudaMemcpyDeviceToHost);
+
+    cudaFree(dInput);
+    cudaFree(dOutput);
+    
+    return result;
 }
--- 3821B1PE1/2_gelu_cuda/khramov_ivan/gelu_cuda.h
+++ 3821B1PE1/2_gelu_cuda/khramov_ivan/gelu_cuda.h
@@ -1,3 +1,4 @@
+// Copyright (c) 2024 Khramov Ivan
 #ifndef __GELU_CUDA_H
 #define __GELU_CUDA_H
 
