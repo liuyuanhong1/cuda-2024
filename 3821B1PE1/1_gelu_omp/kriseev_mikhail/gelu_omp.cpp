5be19733a4ce3ad6240fd2845a78afbad9bd01e8
MadKris <mkris.yt@gmail.com>
2024-11-02 20:23:49 +0300

    Лабораторная работа № 1

---
+++ 3821B1PE1/1_gelu_omp/kriseev_mikhail/gelu_omp.cpp
@@ -0,0 +1,23 @@
+#include "gelu_omp.hpp"
+#include <cmath>
+
+#define GELU_COEF1 1.595769122f
+#define GELU_COEF2 0.071354816f
+
+std::vector<float> GeluOMP(const std::vector<float>& input) {
+    std::vector<float> output(input.size());
+
+    const int size = input.size();
+
+    const float* input_data = input.data();
+    float* output_data = output.data();
+
+    #pragma omp parallel for private(size)
+    for (int i = 0; i < size; i++) {
+        float x = input_data[i];
+        
+        output_data[i] = x * (1.0f - 1.0f / (1.0f + std::expf(x * (GELU_COEF1 + x * x * GELU_COEF2)))); 
+    }
+
+    return output;
+}
