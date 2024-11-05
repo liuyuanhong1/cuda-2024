18ac79c7a57972ea92b9fb267a078845f2cdc23d
PavelLSmirnov <pavel1201s@yandex.ru>
2024-11-05 18:35:03 +0300

    fixes

--- 3821B1PE3/9_gelu_ocl/smirnov_pavel/gelu_ocl.cpp
+++ 3821B1PE3/9_gelu_ocl/smirnov_pavel/gelu_ocl.cpp
@@ -1,59 +1,80 @@
-#include <cmath>
-#include <cstddef>
+#include "gelu_ocl.h"
+
+#include <CL/opencl.hpp>
 #include <iostream>
 
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-#include "cufft.h"
-
-#include "fft_cufft.h"
-
-#define CUDA_CALL(status)                                     \
-  {                                                           \
-    if (status != cudaSuccess) {                              \
-      std::cout << "error " << __FILE__ << ":" << __LINE__;   \
-      std::cout << " " << cudaGetErrorString(status) << "\n"; \
-      std::exit(0);                                           \
-    }                                                         \
+#define CHECK_CL_ERROR(callable)                                          \
+  {                                                                       \
+    auto codeError = callable;                                            \
+    if (codeError != CL_SUCCESS) {                                        \
+      std::cerr << "\033[1;31merror\033[0m: ";                            \
+      std::cerr << "code error: " << static_cast<int>(codeError) << '\n'; \
+      std::cerr << "loc: " << __FILE__ << '(' << __LINE__ << ")\n";       \
+      std::exit(static_cast<int>(codeError));                             \
+    }                                                                     \
   }
 
-#define CUFFT_CALL(status)                                          \
-  {                                                                 \
-    if (status != CUFFT_SUCCESS) {                                  \
-      std::cout << "error " << __FILE__ << ":" << __LINE__ << "\n"; \
-      std::exit(0);                                                 \
-    }                                                               \
+std::vector<float> GeluOCL(const std::vector<float>& input) {
+  std::vector<cl::Platform> platforms;
+  cl::Platform::get(&platforms);
+  if (platforms.empty()) {
+    std::cerr << "\033[1;31merror\033[0m: no platform available\n";
+    return {};
+  }
+  cl::Platform platform = platforms.front();
+
+  std::vector<cl::Device> devices;
+  platform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
+  if (devices.empty()) {
+    std::cerr << "\033[1;31merror\033[0m: no device available\n";
+    return {};
+  }
+  cl::Device device = devices.front();
+
+  cl::Context context(device);
+  cl::CommandQueue queue(context);
+
+  std::string codeKernel = R"(
+__kernel void gelu_kernel(__global const float* x, __global float* y, int countElem) {
+  int i = get_global_id(0);
+  if (i < countElem) {
+      float val = x[i];
+      float tmp = val * (1.595769122f + val * val * 0.071354816f);
+      y[i] = val - val / (1.0f + exp(tmp));
+  }
+}
+)";
+
+  cl::Program::Sources sources;
+  sources.emplace_back(std::move(codeKernel));
+
+  cl::Program program(context, sources);
+  if (program.build() != CL_SUCCESS) {
+    std::cerr << "\033[1;31merror\033[0m: ";
+    std::cerr << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device) << '\n';
+    return {};
   }
 
-std::vector<float> FffCUFFT(const std::vector<float>& input, int batch) {
-  size_t size = input.size();
-  size_t byte_count = size * sizeof(float);
-  size_t batch_size = size / batch / 2;
+  if (input.empty()) return {};
+  auto size = input.size();
+  auto countBytes = size * sizeof(float);
 
-  cufftComplex* dev_input_mem = nullptr;
-  std::vector<float> result(size);
+  cl::Buffer bufferInput(context, CL_MEM_READ_ONLY, countBytes);
+  cl::Buffer bufferOutput(context, CL_MEM_WRITE_ONLY, countBytes);
 
-  CUDA_CALL(cudaMalloc(reinterpret_cast<void**>(&dev_input_mem), byte_count));
+  CHECK_CL_ERROR(queue.enqueueWriteBuffer(bufferInput, CL_TRUE, 0, countBytes,
+                                          input.data()));
 
-  CUDA_CALL(cudaMemcpy(dev_input_mem, input.data(), byte_count,
-                       cudaMemcpyHostToDevice));
+  cl::Kernel kernel(program, "gelu_kernel");
+  kernel.setArg(0, bufferInput);
+  kernel.setArg(1, bufferOutput);
+  kernel.setArg(2, static_cast<int>(size));
+  CHECK_CL_ERROR(queue.enqueueNDRangeKernel(kernel, cl::NullRange,
+                                            cl::NDRange(size), cl::NullRange));
 
-  cufftHandle plan;
+  std::vector<float> output(size);
+  CHECK_CL_ERROR(queue.enqueueReadBuffer(bufferOutput, CL_TRUE, 0, countBytes,
+                                         output.data()));
 
-  CUFFT_CALL(cufftPlan1d(&plan, batch_size, CUFFT_C2C, batch));
-  CUFFT_CALL(cufftExecC2C(plan, dev_input_mem, dev_input_mem, CUFFT_FORWARD));
-  CUFFT_CALL(cufftExecC2C(plan, dev_input_mem, dev_input_mem, CUFFT_INVERSE));
-
-  CUDA_CALL(cudaMemcpy(reinterpret_cast<void*>(result.data()),
-                       reinterpret_cast<void*>(dev_input_mem), byte_count,
-                       cudaMemcpyDeviceToHost));
-
-  for (size_t i = 0; i < size; i++) {
-    result[i] /= static_cast<float>(batch_size);
-  }
-
-  cufftDestroy(plan);
-  CUDA_CALL(cudaFree(reinterpret_cast<void*>(dev_input_mem)));
-
-  return result;
+  return output;
 }
