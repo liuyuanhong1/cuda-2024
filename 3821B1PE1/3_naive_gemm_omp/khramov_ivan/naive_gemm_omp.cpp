d46684b3eb010df385e39eb37d2ebbdb2b09383f
At0mn1yIvan <khramov.vanya@bk.ru>
2024-11-12 20:36:11 +0300

    Style fix.

--- 3821B1PE1/3_naive_gemm_omp/khramov_ivan/naive_gemm_omp.cpp
+++ 3821B1PE1/3_naive_gemm_omp/khramov_ivan/naive_gemm_omp.cpp
@@ -5,20 +5,20 @@
 std::vector<float> NaiveGemmOMP(const std::vector<float>& a,
                                 const std::vector<float>& b,
                                 int n) {
-  int elemCount = n * n;
-  if (a.size() != elemCount || b.size() != elemCount) return {};
+    int elemCount = n * n;
+    if (a.size() != elemCount || b.size() != elemCount) return {};
 
-  std::vector<float> c(elemCount, 0.0f);
+    std::vector<float> c(elemCount, 0.0f);
 
 #pragma omp parallel for collapse(2)
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      float sum = 0.0f;
-      for (int k = 0; k < n; ++k) {
-        sum += a[i * n + k] * b[n * k + j];
-      }
-      c[i * n + j] = sum;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            float sum = 0.0f;
+            for (int k = 0; k < n; ++k) {
+                sum += a[i * n + k] * b[n * k + j];
+            }
+            c[i * n + j] = sum;
+        }
     }
-  }
-  return c;
+    return c;
 }
