2a408f1cfb33b9881f3087ad4af2706407c3b642
Andrey Chuvashov <ch.andrey.2345@gmail.com>
2024-11-15 17:40:04 +0300

    fix: try again...

--- 3821B1PE1/1_gelu_omp/chuvashov_andrey/gelu_omp.cpp
+++ 3821B1PE1/1_gelu_omp/chuvashov_andrey/gelu_omp.cpp
@@ -8,7 +8,6 @@ const float pi_c = sqrt(2.0f / (2 * asin(1.0f))); // sqrt(2 / pi)
 const float par_c= 0.044715f; // coefficient in parentheses
 
 std::vector<float> GeluOMP(const std::vector<float>& input) {
-
     const size_t length = input.size();
     std::vector<float> result(length);
 
