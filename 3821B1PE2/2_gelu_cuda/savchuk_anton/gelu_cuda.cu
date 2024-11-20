9dc9da52d1a3b33002a2728365fa6a8073fd0282
savchukPR2 <anton.s.savchuk@gmail.com>
2024-11-20 15:03:37 +0300

    2_gelu_cuda task

---
+++ 3821B1PE2/2_gelu_cuda/savchuk_anton/gelu_cuda.cu
@@ -0,0 +1,45 @@
+﻿#include "gelu_cuda.h"
+#include <cuda_runtime.h>
+#include <vector>
+#include <cmath>
+#include <stdexcept>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846 // Определение числа π
+#endif
+
+__global__ void GeluKernel(const float* input, float* output, int size) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < size) {
+        float x = input[idx];
+        output[idx] = 0.5f * x * (1.0f + tanhf(sqrtf(2.0f / M_PI) * (x + 0.044715f * x * x * x)));
+    }
+}
+
+std::vector<float> GeluCUDA(const std::vector<float>& input) {
+    int size = input.size();
+
+    if (size == 0) {
+        return {};
+    }
+
+    float* d_input;
+    float* d_output;
+    cudaMalloc(&d_input, size * sizeof(float));
+    cudaMalloc(&d_output, size * sizeof(float));
+
+    cudaMemcpy(d_input, input.data(), size * sizeof(float), cudaMemcpyHostToDevice);
+
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (size + threadsPerBlock - 1) / threadsPerBlock;
+
+    GeluKernel << <blocksPerGrid, threadsPerBlock >> > (d_input, d_output, size);
+
+    std::vector<float> output(size);
+    cudaMemcpy(output.data(), d_output, size * sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaFree(d_input);
+    cudaFree(d_output);
+
+    return output;
+}
