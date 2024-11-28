42319f59a414fa898206df8299099e40be9929ce
unknown <alexander7110230@gmail.com>
2024-11-28 22:36:13 +0300

    commit3_task5

--- 3821B1FI1/5_block_gemm_omp/kashirin_alexander/block_gem_omp.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BLOCK_GEMM_OMP_H
-#define __BLOCK_GEMM_OMP_H
-
-#include <vector>
-
-std::vector<float> BlockGemmOMP(const std::vector<float>& a,
-    const std::vector<float>& b, int size);
-
-#endif  // __BLOCK_GEMM_OMP_H
