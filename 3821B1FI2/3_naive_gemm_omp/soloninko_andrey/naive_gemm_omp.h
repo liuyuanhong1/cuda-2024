6ee1fa78d62f14679800dc3e753bafee330b14ca
Manymoera <soloninko80@gmail.com>
2024-12-04 16:26:41 +0300

    initial commit

---
+++ 3821B1FI2/3_naive_gemm_omp/soloninko_andrey/naive_gemm_omp.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 Soloninko Andrey
+#ifndef __NAIVE_GEMM_OMP_H
+#define __NAIVE_GEMM_OMP_H
+
+#include <vector>
+
+std::vector<float> NaiveGemmOMP(const std::vector<float>& a,
+                                const std::vector<float>& b, int size);
+
+#endif  // __NAIVE_GEMM_OMP_H
