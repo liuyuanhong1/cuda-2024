2911a37e9faa3127cb6a074962328b138b8bc303
ZhatkinVyacheslav <90867068+ZhatkinVyacheslav@users.noreply.github.com>
2024-12-04 22:04:37 +0300

    1_gelu_omp task

--- 3821B1PE2/1_gelu_omp/zhatkin_vyacheslav/gelu_omp.cpp
+++ 3821B1PE2/1_gelu_omp/zhatkin_vyacheslav/gelu_omp.cpp
@@ -1,16 +1,21 @@
 #include "gelu_omp.h"
-#include <cmath>      // Для std::tanh и std::sqrt
-#include <omp.h>      // Для OpenMP
-
-#define M_PI 3.14
+#include <cmath>
+#include <omp.h>
 
 std::vector<float> GeluOMP(const std::vector<float>& input) {
-    std::vector<float> output(input.size()); // Результирующий вектор такого же размера, как входной
+    size_t size = input.size();
+    std::vector<float> output(size);
+
+    const float sqrt_2_over_pi = std::sqrt(2.0f / static_cast<float>(M_PI));
+    const float coeff = 0.044715f;
 
     #pragma omp parallel for
-    for (int i = 0; i < static_cast<int>(input.size()); ++i) { // Используйте int вместо size_t
+    for (long i = 0; i < static_cast<long>(size); ++i) {
         float x = input[i];
-        output[i] = 0.5f * x * (1.0f + std::tanh(std::sqrt(2.0f / M_PI) * (x + 0.044715f * x * x * x)));
+        float x_cubed = x * x * x;
+        float tanh_arg = sqrt_2_over_pi * (x + coeff * x_cubed);
+        float tanh_val = std::tanh(tanh_arg);
+        output[i] = 0.5f * x * (1.0f + tanh_val);
     }
 
     return output;
