3650270b9e5d49e5001d954b2b7ae42b2295da53
Stepan Morgachev <stepan.morgachev@gmail.com>
2024-12-05 14:37:05 +0300

    feat: added task6 solution

---
+++ 3821B1PE1/6_block_gemm_cuda/morgachev_stepan/block_gemm_cuda.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2024 Morgachev Stepan
+#ifndef __BLOCK_GEMM_CUDA_H
+#define __BLOCK_GEMM_CUDA_H
+
+#include <vector>
+
+std::vector<float> BlockGemmCUDA(const std::vector<float>& a,
+    const std::vector<float>& b,
+    int n);
+
+#endif // __BLOCK_GEMM_CUDA_H
