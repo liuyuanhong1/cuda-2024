4a22f324525ccc493d356e52d67eb80cfd209857
aartyomm <artem-kostin2013.kostin@yandex.ru>
2024-12-14 17:32:43 +0300

    9_gelu_ocl 2nd

--- 3821B1FI2/9_gelu_ocl/kostin_artem/gelu_ocl.cpp
+++ 3821B1FI2/9_gelu_ocl/kostin_artem/gelu_ocl.cpp
@@ -1,4 +1,5 @@
-#include <CL/cl.hpp>
+#include "gelu_ocl.h"
+#include <CL/opencl.hpp>
 #include <vector>
 #include <cmath>
 #include <iostream>
@@ -6,16 +7,16 @@
 #define GELU(x) ((x) * 0.5 * (1.0 + tanh(sqrt(2.0 / M_PI) * ((x) + 0.044715 * (x) * (x) * (x)))))
 
 std::vector<float> GeluOCL(const std::vector<float>& input) {
-    cl_int err;
-    cl::Platform platform;
-    cl::Device device;
-    cl::Context context;
-    cl::CommandQueue queue;
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+    cl::Platform platform = platforms.front();
 
-    err = cl::Platform::get(&platform);
-    err |= platform.getDevices(CL_DEVICE_GPU, &device);
-    context = cl::Context(device);
-    queue = cl::CommandQueue(context, device);
+    std::vector<cl::Device> devices;
+    platform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
+    cl::Device device = devices.front();
+
+    cl::Context context(device);
+    cl::CommandQueue queue(context);
 
     const char* kernelSource = R"(
         __kernel void gelu_kernel(__global float* input, __global float* output, int size) {
@@ -27,19 +28,25 @@ std::vector<float> GeluOCL(const std::vector<float>& input) {
         }
     )";
 
-    cl::Program program(context, kernelSource);
-    program.build("-cl-std=CL1.2");
+    cl::Program::Sources sources(1, std::make_pair(kernelSource, strlen(kernelSource)));
+    cl::Program program(context, sources);
+    program.build({device});
 
-    cl::Buffer inputBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, input.size() * sizeof(float), (void*)input.data());
-    cl::Buffer outputBuffer(context, CL_MEM_WRITE_ONLY, input.size() * sizeof(float));
+    auto size = input.size();
+    auto countBytes = size * sizeof(float);
+
+    cl::Buffer inputBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, countBytes, (void*)input.data());
+    cl::Buffer outputBuffer(context, CL_MEM_WRITE_ONLY, countBytes);
 
     cl::Kernel kernel(program, "gelu_kernel");
     kernel.setArg(0, inputBuffer);
     kernel.setArg(1, outputBuffer);
-    kernel.setArg(2, (int)input.size());
+    kernel.setArg(2, static_cast<int>(size));
 
-    queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(input.size()), cl::NDRange(256));
-    queue.enqueueReadBuffer(outputBuffer, CL_TRUE, 0, input.size() * sizeof(float), output.data());
+    queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(size), cl::NullRange);
+
+    std::vector<float> output(size);
+    queue.enqueueReadBuffer(outputBuffer, CL_TRUE, 0, countBytes, output.data());
 
     return output;
 }
