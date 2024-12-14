82db5f257b8ce0d5a9f63fa5fd7ae6db108c64a6
Stepa123de <stepa.kashin.2018@gmail.com>
2024-12-14 18:17:58 +0300

    +

--- 3821B1PE1/1_gelu_omp/kashin_stepan/gelu_omp.cpp
+++ 3821B1PE1/1_gelu_omp/kashin_stepan/gelu_omp.cpp
@@ -1,25 +1,21 @@
-// Copyright (c) 2024 Kashin Stepan
-// Разрешаю копировать и взаимствовать, если указан оригинальный автор: "// Copyright (c) 2024 Kashin Stepan -> Ivanov Ivan"
 #include "gelu_omp.h"
+#include <cmath>
 
-#include <math.h>
-#include <vector>
-#include <omp.h>
 
-// sqrt(pi/2)
-const float a = 0.7978845608f;
-// cdf_coefficient
-const float b = 0.044715f;
+std::vector<float> GeluOMP(const std::vector<float> &input) {
+  if (input.empty()) return {};
 
-std::vector<float> GeluOMP(const std::vector<float>& input) {
-  const size_t range = input.size();
-  std::vector<float> answer(range);
+  constexpr float geluCoeff1 = 1.595769122f;
+  constexpr float geluCoeff2 = 0.071354816f;    
 
-  #pragma omp parallel for
-  for (size_t i = 0; i < range; i++) {
-    float x = input[i];
-    answer[i] = 0.5f * x * (1.0f + tanhf(a * (x + b * x * x * x)));
+  auto size = input.size();
+  std::vector<float> output(size);
+
+#pragma omp parallel for
+  for (size_t i = 0; i < size; ++i) {
+    float value = input[i];
+    output[i] = value * (1 - 1 / (1.0f + std::exp(value * (geluCoeff1 + value * value * geluCoeff2))));
   }
 
-  return answer;
+  return output;
 }
