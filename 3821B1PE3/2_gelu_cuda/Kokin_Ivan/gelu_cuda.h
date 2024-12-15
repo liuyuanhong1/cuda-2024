db0b65eac5a4133752e4c85cf6d4f07756a7d6d0
ThreeToads <ivan.kokin.465@gmail.com>
2024-12-16 02:48:06 +0300

    fix(lab2): delete 2 files for solution

--- 3821B1PE3/2_gelu_cuda/Kokin_Ivan/gelu_cuda.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024 Kokin Ivan
-#ifndef __GELU_CUDA_H
-#define __GELU_CUDA_H
-
-#include <vector>
-
-std::vector<float> GeluCUDA(const std::vector<float>& input);
-
-#endif // __GELU_CUDA_H
