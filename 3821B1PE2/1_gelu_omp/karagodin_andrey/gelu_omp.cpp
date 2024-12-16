1e943521e23488c387155616ed77e394e008724f
MineShadow7 <shad-karagodin@ya.ru>
2024-12-16 18:36:05 +0300

    Define Headers

---
+++ 3821B1PE2/1_gelu_omp/karagodin_andrey/gelu_omp.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 Karagodin-Andrey
+#include "gelu_omp.h"
+
+#include <cmath>
+
+std::vector<float> GeluOMP(const std::vector<float> &input) {
+  auto size = input.size();
+  std::vector<float> output(size);
+  return;
+}
