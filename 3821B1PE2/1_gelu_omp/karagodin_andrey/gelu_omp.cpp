9dc4733d69cb8dd1a498373d93ec560251c6d307
MineShadow7 <shad-karagodin@ya.ru>
2024-12-16 18:37:31 +0300

    Task done

--- 3821B1PE2/1_gelu_omp/karagodin_andrey/gelu_omp.cpp
+++ 3821B1PE2/1_gelu_omp/karagodin_andrey/gelu_omp.cpp
@@ -4,7 +4,28 @@
 #include <cmath>
 
 std::vector<float> GeluOMP(const std::vector<float> &input) {
+  if (input.empty()) return {};
+
+  /**
+   * PI = 3.14159
+   * constOne = 2 * sqrt(2 / PI)
+   * constTwo = constOne * 0.044715
+   * tmp = x * (constOne + x * x * constTwo)
+   * result = x - x / (1 + exp(tmp))
+   */
+
+  constexpr float constOne = 1.595769122f;
+  constexpr float constTwo = constOne * 0.044715f;
+
   auto size = input.size();
   std::vector<float> output(size);
-  return;
+
+#pragma omp parallel for
+  for (size_t i = 0; i < size; ++i) {
+    float val = input[i];
+    float tmp = val * (constOne + val * val * constTwo);
+    output[i] = val - val / (1.0f + std::exp(tmp));
+  }
+
+  return output;
 }
