635fc9f2268b866f578cba7b37ebf492ac210712
MineShadow7 <shad-karagodin@ya.ru>
2024-12-16 21:24:55 +0300

    Prepare code for task

---
+++ 3821B1PE2/4_naive_gemm_cuda/karagodin_andrey/naive_gemm_cuda.cu
@@ -0,0 +1,8 @@
+// Copyright (c) 2024 Karagodin Andrey
+#include "naive_gemm_cuda.h"
+
+std::vector<float> NaiveGemmCUDA(const std::vector<float>& a,
+                                 const std::vector<float>& b,
+                                 int n) {
+    // Place your implementation here
+}
