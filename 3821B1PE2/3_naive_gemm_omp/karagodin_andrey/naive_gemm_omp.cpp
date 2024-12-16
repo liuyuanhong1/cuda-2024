a56309e2bce4326b2fd5dca1c516f80cd9cd6aea
MineShadow7 <shad-karagodin@ya.ru>
2024-12-16 18:47:46 +0300

    Initial task done

--- 3821B1PE2/3_naive_gemm_omp/karagodin_andrey/naive_gemm_omp.cpp
+++ 3821B1PE2/3_naive_gemm_omp/karagodin_andrey/naive_gemm_omp.cpp
@@ -2,7 +2,17 @@
 #include "naive_gemm_omp.h"
 
 std::vector<float> NaiveGemmOMP(const std::vector<float>& a,
-                                const std::vector<float>& b,
-                                int n) {
-    // Place your implementation here
+                                const std::vector<float>& b, int size) {
+  auto countElem = size * size;
+  if (a.size() != countElem || b.size() != countElem) return {};
+
+  std::vector<float> c(countElem, 0.0f);
+
+#pragma omp parallel for shared(a, b, c)
+  for (int m = 0; m < size; ++m)
+    for (int n = 0; n < size; ++n)
+      for (int k = 0; k < size; ++k)
+        c[m * size + n] += a[m * size + k] * b[size * k + n];
+
+  return c;
 }
