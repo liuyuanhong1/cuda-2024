0cdd4f8355dded4f192a03cf0552eaf6ec261c9c
MineShadow7 <shad-karagodin@ya.ru>
2024-12-17 19:29:01 +0300

    Initialize cuda task

---
+++ 3821B1PE2/6_block_gemm_cuda/karagodin_andrey/block_gemm_cuda.cu
@@ -0,0 +1,8 @@
+//  Copyright (c) 2024 Karagodin Andrey
+#include "block_gemm_cuda.h"
+
+std::vector<float> BlockGemmCUDA(const std::vector<float>& a,
+                                 const std::vector<float>& b,
+                                 int n) {
+    // Place your implementation here
+}
