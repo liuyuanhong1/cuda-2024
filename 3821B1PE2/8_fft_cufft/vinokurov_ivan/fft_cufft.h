0aab7495c3806260d5eb02cf18930864a7bb8fc6
Ivan Vinokurov <39878097+LeakMachine@users.noreply.github.com>
2024-12-17 20:12:18 +0300

    8_fft_cufft task

---
+++ 3821B1PE2/8_fft_cufft/vinokurov_ivan/fft_cufft.h
@@ -0,0 +1,9 @@
+//  Copyright (c) 2024 Vinokurov Ivan
+#ifndef __FFT_CUFFT_H
+#define __FFT_CUFFT_H
+
+#include <vector>
+
+std::vector<float> FffCUFFT(const std::vector<float>& input, int batch);
+
+#endif // __FFT_CUFFT_H
