66716af9a859b5966dd67d45da009d8f14922e71
MineShadow7 <shad-karagodin@ya.ru>
2024-12-17 19:38:19 +0300

    Init branch and initial task files

---
+++ 3821B1PE2/8_fft_cufft/karagodin_andrey/fft_cufft.cu
@@ -0,0 +1,6 @@
+// Copyright (c) 2024 Karagodin Andrey 
+#include "fft_cufft.h"
+
+std::vector<float> FffCUFFT(const std::vector<float>& input, int batch) {
+    // Place your implementation here
+}
