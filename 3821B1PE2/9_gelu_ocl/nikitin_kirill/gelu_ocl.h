3793e99fe0f5853b51c6304468e4ff929eae95a4
NikitinKU <clownzzzz@mail.ru>
2024-12-19 00:18:59 +0300

    9_gelu_ocl Nikitin Kirill PR2

---
+++ 3821B1PE2/9_gelu_ocl/nikitin_kirill/gelu_ocl.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 Nikitin Kirill
+
+#ifndef __GELU_OCL_H
+#define __GELU_OCL_H
+
+#include <vector>
+
+std::vector<float> GeluOCL(const std::vector<float>& input);
+
+#endif // __GELU_OCL_H
