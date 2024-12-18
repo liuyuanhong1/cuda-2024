06d4679e42ad8d3d25a2d7ab957ed280b1266537
AleksandrKutarin <fames5369@gmail.com>
2024-12-18 23:14:30 +0300

    add lab9

---
+++ 3821B1PE2/9_gelu_ocl/kutarin_aleksandr/gelu_ocl.h
@@ -0,0 +1,10 @@
+//Copyright Kutarin Aleksandr 2024
+
+#ifndef __GELU_OCL_H
+#define __GELU_OCL_H
+
+#include <vector>
+
+std::vector<float> GeluOCL(const std::vector<float>& input);
+
+#endif // __GELU_OCL_H
