01494a1ab918b3ce127c7ede107ff13ab7624fa5
Ivan Novostroev <54308529+GERONlMO@users.noreply.github.com>
2024-12-19 02:09:06 +0300

    Лабораторная работа №7. Новостроев Иван, 3821Б1ПР2
    lab7

---
+++ 3821B1PE2/7_gemm_cublas/novostroev_ivan/gemm_cublas.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2024 Novostroev Ivan
+
+#ifndef __GEMM_CUBLAS_H
+#define __GEMM_CUBLAS_H
+
+#include <vector>
+
+std::vector<float> GemmCUBLAS(const std::vector<float>& a,
+                              const std::vector<float>& b,
+                              int size);
+
+#endif // __GEMM_CUBLAS_H
