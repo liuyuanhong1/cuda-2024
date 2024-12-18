61f89cf21d47a1230498a1c6ce3b0569e0c722df
NikitinKU <clownzzzz@mail.ru>
2024-12-18 23:57:53 +0300

    4_naive_gemm_cuda Nikitin Kirill PR2

---
+++ 3821B1PE2/4_naive_gemm_cuda/nikitin_kirill/naive_gemm_cuda.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2024 Nikitin Kirill
+
+#ifndef __NAIVE_GEMM_CUDA_H
+#define __NAIVE_GEMM_CUDA_H
+
+#include <vector>
+
+std::vector<float> NaiveGemmCUDA(const std::vector<float>& a,
+                                 const std::vector<float>& b, int size);
+
+std::vector<float> NaiveGemmCUDA_v2(const std::vector<float>& a,
+                                 const std::vector<float>& b, int size);
+
+#endif  // __NAIVE_GEMM_CUDA_H
