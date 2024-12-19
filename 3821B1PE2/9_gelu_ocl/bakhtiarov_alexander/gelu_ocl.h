5f8ad596972fe59b741b063e50394b0179dcbb5d
Firstbourne <illvibes11moj@gmail.com>
2024-12-19 22:25:19 +0300

    add lab 9

---
+++ 3821B1PE2/9_gelu_ocl/bakhtiarov_alexander/gelu_ocl.h
@@ -0,0 +1,10 @@
+// Copyright 2024 Bakhtiarov Alexander
+
+#ifndef __GELU_OCL_H
+#define __GELU_OCL_H
+
+#include <vector>
+
+std::vector<float> GeluOCL(const std::vector<float>& input);
+
+#endif // __GELU_OCL_H
