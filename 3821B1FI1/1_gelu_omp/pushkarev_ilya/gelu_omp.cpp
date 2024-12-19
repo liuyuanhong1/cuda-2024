1cdcb204b04c9eae03822133849b5976a423fd87
user <ipnnru@gmail.com>
2024-12-19 20:41:45 +0300

    v1

---
+++ 3821B1FI1/1_gelu_omp/pushkarev_ilya/gelu_omp.cpp
@@ -0,0 +1,24 @@
+#include "gelu_omp.h"
+
+#include <cmath>
+
+std::vector<float> GeluOMP(const std::vector<float>& input) {
+
+    auto size_input = input.size();
+
+    if (size_input == 0) {
+        return {};
+    }
+
+     constexpr float temp = 0.7978845608f;
+
+    std::vector<float> result(size_input);
+
+#pragma omp parallel for
+    for (size_t i = 0; i < size_input; ++i) {
+        float x = input[i];
+        result[i] = 0.5f * x * (1.f + tanh(temp * (x + 0.044715f * x * x * x)));
+    }
+
+    return result;
+}
