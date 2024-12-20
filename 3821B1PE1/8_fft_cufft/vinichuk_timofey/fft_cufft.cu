264f6f13c998e5115b06a3d8089ed00b6a131051
V-inTim <vinichuk.timofey@mail.ru>
2024-12-20 21:54:37 +0300

    Fix.

--- 3821B1PE1/8_fft_cufft/vinichuk_timofey/fft_cufft.cu
+++ 3821B1PE1/8_fft_cufft/vinichuk_timofey/fft_cufft.cu
@@ -1,52 +1,59 @@
 // Copyright (c) 2024 Vinichuk Timofey
 #include "fft_cufft.h"
 
-__global__ void normalizeKernel(float* input, int size, int n) {
-	int index = blockIdx.x * blockDim.x + threadIdx.x;
-	if (index < size) {
-		input[index] /= n;
-	}
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <cufft.h>
+
+__global__ void NormalizeKernel(float* input, int size, int N) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    if (index < size) {
+        input[index] /= N;
+    }
 }
 
 std::vector<float> FffCUFFT(const std::vector<float>& input, int batch) {
-	std::vector<float> output(input.size());
+    std::vector<float> result(input.size());
 
-	int n = input.size() / (batch * 2);
+    int N = input.size() / (batch * 2);
 
-	cufftHandle handle;
-	cufftPlan1d(&handle, n, CUFFT_C2C, batch);
+    cufftHandle handle;
+    cufftComplex* data;
 
-	cufftComplex* data;
-	cudaMalloc(&data, sizeof(cufftComplex) * n * batch);
-	cudaMemcpy(
-		data,
-		input.data(),
-		sizeof(cufftComplex) * n * batch,
-		cudaMemcpyHostToDevice
-	);
+    cufftPlan1d(&handle, N, CUFFT_C2C, batch);
 
-	cufftExecC2C(handle, data, data, CUFFT_FORWARD);
-	cufftExecC2C(handle, data, data, CUFFT_INVERSE);
+    cudaMalloc(&data, sizeof(cufftComplex) * N * batch);
+    cudaMemcpy(
+        data,
+        input.data(),
+        sizeof(cufftComplex) * N * batch,
+        cudaMemcpyHostToDevice
+    );
 
-	cudaDeviceProp devPropts;
-	cudaGetDeviceProperties(&devPropts, 0);
-	size_t threadsPerBlock = devPropts.maxThreadsPerBlock;
-	size_t blocksCount = (input.size() + threadsPerBlock - 1) / threadsPerBlock;
+    cufftExecC2C(handle, data, data, CUFFT_FORWARD);
+    cufftExecC2C(handle, data, data, CUFFT_INVERSE);
 
-	normalizeKernel << <blocksCount, threadsPerBlock >> > (
-		reinterpret_cast<float*>(data),
-		output.size(),
-		n);
+    cudaDeviceProp devPropts;
+    cudaGetDeviceProperties(&devPropts, 0);
+    size_t threadsPerBlock = devPropts.maxThreadsPerBlock;
+    size_t blocksCount = (input.size() + threadsPerBlock - 1) / threadsPerBlock;
 
-	cudaMemcpy(
-		output.data(),
-		data,
-		sizeof(cufftComplex) * n * batch,
-		cudaMemcpyDeviceToHost
-	);
+    NormalizeKernel << <blocksCount, threadsPerBlock >> > (
+        reinterpret_cast<float*>(data),
+        result.size(),
+        N
+        );
 
-	cufftDestroy(handle);
-	cudaFree(data);
+    cudaMemcpy(
+        result.data(),
+        data,
+        sizeof(cufftComplex) * N * batch,
+        cudaMemcpyDeviceToHost
+    );
 
-	return output;
+    cufftDestroy(handle);
+    cudaFree(data);
+
+    return result;
 }
