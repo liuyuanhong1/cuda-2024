8b11222765014c09ed8ec7e0ce41fb9a27f6902b
Nilemos <you@example.com>
2024-12-21 01:00:27 +0300

    lab7

---
+++ 3821B1FI1/7_gemm_cublas/bozin_dmitry/gemm_cublas.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2024 Bozin Dmitry
+#ifndef __GEMM_CUBLAS_H
+#define __GEMM_CUBLAS_H
+
+#include <vector>
+
+std::vector<float> GemmCUBLAS(const std::vector<float>& a,
+                              const std::vector<float>& b,
+                              int n);
+
+#endif // __GEMM_CUBLAS_H
