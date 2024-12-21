b0fb890bc2890bd71e65b834b4ac3f7915e5f437
JankoZ <maxsya2012@mail.ru>
2024-12-21 13:57:07 +0300

    Fix: task fixed

--- 3821B1FI2/1_gelu_omp/zawadowski_jan/gelu_omp.cpp
+++ 3821B1FI2/1_gelu_omp/zawadowski_jan/gelu_omp.cpp
@@ -1,4 +1,7 @@
 #include "gelu_omp.h"
+#include <vector>
+#include <cmath>
+#include <omp.h>
 
 std::vector<float> GeluOMP(const std::vector<float>& input) {
     std::vector<float> output(input.size());
@@ -6,8 +9,8 @@ std::vector<float> GeluOMP(const std::vector<float>& input) {
     #pragma omp parallel for
     for (size_t i = 0; i < input.size(); ++i) {
         float x = input[i];
-        float hyperbolicTan = std::tanh(0.7978845608f * (x + 0.044715f * x * x * x));
-        output[i] = 0.5f * x * (1.0f + hyperbolicTan);
+        float hyperTan = std::tanh(0.7978845608f * (x + 0.044715f * x * x * x));
+        output[i] = 0.5f * x * (1.0f + hyperTan);
     }
 
     return output;
